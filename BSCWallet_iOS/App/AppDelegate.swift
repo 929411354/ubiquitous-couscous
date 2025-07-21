9dee1bf690a3786e2053af7e4ebcb061448be682
929411354 <136059659+929411354@users.noreply.github.com>
2025-07-21 18:12:21 +0800

    更新 AppDelegate.swift

--- BSCWallet_iOS/App/AppDelegate.swift
+++ BSCWallet_iOS/App/AppDelegate.swift
@@ -3,8 +3,9 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // 初始化钱包服务
+        // 初始化钱包和代币服务
         WalletManager.shared.setup()
+        TokenManager.shared.setup()
         return true
     }
 }
